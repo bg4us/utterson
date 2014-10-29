a58f0b3370dc81bd913ffe77b588d13febc8f37b
Juan C. Méndez <jcmendez@alum.mit.edu>
2014-10-28 23:39:59 -0400

    Initial check in
    Signed-off-by: Juan C. Méndez <jcmendez@alum.mit.edu>

---
+++ README.md
@@ -0,0 +1,12 @@
+Utterson - contact form processing
+----------------------------------
+
+This is an open source tool for static sites (Jekyll, etc.) where you want 
+to have a simple contact form that will be forwarded to your email.
+
+We developed this for our own sites, and you are welcome to use our running
+instance in heroku by registering.  You can inspect the code on github.  We
+promise, we are not going to spam you, use the email addresses you collect,
+or do anything evil.  Inspect the code on github, or if you still feel uneasy
+about using our server, create a free Heroku account and roll your own.
+
---
+++ app.rb
@@ -0,0 +1,77 @@
+require 'moneta'
+require 'uuid'
+require 'pony'
+
+configure :development do
+  set :base_url, 'http://127.0.0.1:5000'
+  app_cache = Moneta.new(:File, dir: 'app_cache')
+  set :app_cache, app_cache
+
+  signup_cache = Moneta.new(:File, dir: 'signup_cache')
+  set :signup_cache, signup_cache
+end
+
+get '/' do
+  erb :index
+end
+
+get '/test' do
+  erb :test
+end
+
+# The signup email will get a something like {'token':'060fdc20-411a-0132-6866-38e856374b1e'}
+post '/setup/:email' do
+  content_type :json
+  email = params[:email]
+  halt 400 if !email
+
+  uuid = UUID.new
+  authkey = uuid.generate
+  settings.signup_cache[email] = authkey
+  Pony.mail(:to => email, 
+          :from => 'utterson@bg4us.com', 
+          :subject => 'Confirm your email', 
+          :body => "Please visit #{base_url}/confirm/#{email}/#{authkey} to get your app token")
+  status 202
+end
+
+get '/confirm/:email/:authkey' do
+  email = params[:email]  
+  authkey = params[:authkey]
+
+  # Fail if no parameters
+  halt 400 if !email || !authkey
+
+  # Fail if the signup token is not found
+  halt 400 unless settings.signup_cache.key?(email)
+  
+  halt 404 if authkey != settings.signup_cache[email]
+
+  # All good, generate an app id and return it
+  uuid = UUID.new
+  appid = uuid.generate
+  settings.app_cache[appid] = email
+  [201, "{'token':'#{appid}'}"]
+end
+
+post '/send/:appid' do
+  appid = params[:appid]
+  halt 400 unless appid
+
+  halt 404 unless settings.app_cache.key?(appid)
+
+  sender = settings.app_cache[appid]
+
+  name = params[:name]
+  email = params[:email]
+  message = params[:message]
+
+  # Fail if no parameters
+  halt 400 if !email || !name || !message
+
+  Pony.mail(:to => email, 
+            :from => sender, 
+            :subject => 'New Message', 
+            :body => "Name:\t#{name}\rEmail:\t#{email}\rMessage\r#{message}")
+end
+
